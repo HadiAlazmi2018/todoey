6cf17dd97ada91f9bd1a246857bcda1337e4493c
hadi alazmi <hadialazmi@hadis-MacBook-Pro.local>
2018-06-01 05:04:40 +0300

    Convert MVC design pattem

---
+++ todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  todoey
+//
+//  Created by hadi alazmi on 5/29/18.
+//  Copyright © 2018 hadi alazmi. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
